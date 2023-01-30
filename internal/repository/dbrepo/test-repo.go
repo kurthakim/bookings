fd1ebf6415db35303ae98e73d181c634329ddc27
Kurt Hakim <kurthakim@gmail.com>
2023-01-31 00:25:07 +0700

    updated tests

--- internal/repository/dbrepo/test-repo.go
+++ internal/repository/dbrepo/test-repo.go
@@ -59,7 +59,10 @@ func (m *testDBRepo) 	UpdateUser(u models.User) error {
 }
 
 func (m *testDBRepo) 	Authenticate(email, testPassword string) (int, string, error) {
-	return 1, "", nil
+	if email == "me@here.ca" {
+		return 1, "", nil
+	}
+	return 0, "", errors.New("some error")
 }
 
 // AllReservations returns a slice of all reservations
